9086245d2c1cf09c9e6c474821b3b5cb3d53f540
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-06-15 12:23:01 +0200

    PRACTICA 2

--- PRACTICA 2/comprueba_ip.py
+++ PRACTICA 2/comprueba_ip.py
@@ -5,3 +5,13 @@ entrada = sys.argv[1:]
 if len (entrada) == 1:
     ip = entrada[0]
     bloques = ip.split('.')
+
+    if len(bloques) == 4:
+        o1 = int(bloques[0])
+        o2 = int(bloques[1])
+        o3 = int(bloques[2])
+        o4 = int(bloques[3])
+
+    if 0 <= o1 <= 255 and 0 <= o2 <= 255 and 0 <= o3 <= 255 and 0 <= o4 <= 255:
+        print("IP válida")
+        
