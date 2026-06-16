a31ada4769e92066a3f1f5e1ae37a0055bd3bb6f
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-06-16 12:42:57 +0200

    PRACTICA 7

--- PRACTICA 7/modulo_python.py
+++ PRACTICA 7/modulo_python.py
@@ -14,4 +14,12 @@ if len(entrada) == 4:
     encontrado1 = False
     archivo1 = zipfile.ZipFile(zip_fuerza)
 
-    
+    for intento in itertools.product(caracteres, repeat=longitud):
+        if encontrado1 == False:
+            clave_bruta = "".join(intento)
+            try:
+                archivo1.extractall(pwd=clave_bruta.encode())
+                print("Fuerza bruta completada, Clave: " + clave_bruta)
+                encontrado1 = True
+            except:
+                pass
