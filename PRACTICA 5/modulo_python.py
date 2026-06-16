0df45737d7174b02ab22e32f40a90517996b011c
HugoBenitezGarcia <hbenagr0801@iesrafaelalberti.es>
2026-06-16 11:13:42 +0200

    PRACTICA 5

---
+++ PRACTICA 5/modulo_python.py
@@ -0,0 +1,19 @@
+import sys
+import socket
+from operator import truediv
+
+entrada = sys.argv[1:]
+
+if len(entrada) == 1 or len(entrada) == 3:
+    ip = entrada[0]
+    bloques = ip.split('.')
+
+    if len(bloques) == 4:
+        o1 = int(bloques[0])
+        o2 = int(bloques[1])
+        o3 = int(bloques[2])
+        o4 = int(bloques[3])
+
+        if 0 <= o1 < 255 and 0 <= o2 < 255 and 0 <= o3 <= 255 and 0 <= o4 <= 255:
+            valido = True
+            puertos = [21, 22, 80, 443, 8080]
